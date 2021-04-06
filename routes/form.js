09c511c5913222c1a3db5c709763a749137418de
Yagnik Moradiya <moradiyayagnik7@gmail.com>
2021-04-06 09:45:39 +0530

    modified form route

--- routes/form.js
+++ routes/form.js
@@ -3,23 +3,27 @@ const router = express.Router();
 
 router.use(express.static("public"));
 
-const student = {
-  id: "19DIT031",
-  name: "MORADIYA YAGNIKKUMAR SHAMBHUBHAI",
-  short_dept: "IT",
-  full_dept: "Information Technology",
-  date: "20/10/2001",
-  start: "11 January 2021",
-  end: "10 May 2021",
-};
-
-router.get("/noc", (req, res) => {
-  res.render("noc", {
-    student,
-  });
+router.post("/noc", (req, res) => {
+  const student = {
+    id: req.body.id,
+    name: req.body.name,
+    short_dept: req.body.short_dept,
+    full_dept: req.body.full_dept,
+    date: req.body.date,
+  };
+  res.render("noc", { student });
 });
 
-router.get("/lor", (req, res) => {
+router.post("/lor", (req, res) => {
+  const student = {
+    id: req.body.id,
+    name: req.body.name,
+    short_dept: req.body.short_dept,
+    full_dept: req.body.full_dept,
+    date: req.body.date,
+    start: req.body.start,
+    end: req.body.end,
+  };
   res.render("lor", {
     student,
   });
